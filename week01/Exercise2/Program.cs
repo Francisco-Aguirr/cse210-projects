3ed78bc296084f202ff45469a4e99030879945c6
francisco aguirre <faguirreramirez@byupathway.edu>
2025-03-05 13:30:42 +0100

    week one exercises

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string userGrade = Console.ReadLine();
+
+        int grade = int.Parse(userGrade);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("Congrats! you grade is: A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Good job! your grade is: B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("You can do it better! your grade is: C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Sorry, You have to focuse next term. Your grade is: D");
+        }
+        else
+        {
+            Console.WriteLine("Try your best next term. Your grade is: F");
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you didn't pass the class.");
+        }
     }
 }
