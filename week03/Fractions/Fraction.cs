e3e640dc2fc563ea9f8dd88b4f917ac2ed4069e4
francisco aguirre <faguirreramirez@byupathway.edu>
2025-03-17 16:08:14 +0100

    encapsulation, getters, setters, constructors, and attributes for a program that performs a fraction operation.

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,68 @@
+using System;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+//Constructor that has no parameters that initializes the number to 1/1.
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+// Constructor that has one parameter for the top and that initializes the denominator to 1.
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+//Constructor that has two parameters, one for the top and one for the bottom.
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // method to print the fraction in format numerator/denominator
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    //Create a method called GetDecimalValue that returns a double that is the result of dividing
+    // the top number by the bottom number, such as 0.75.
+
+    public double GetDecimaValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+
+
+    // Create getters and setters for both the top and the bottom values.
+    // getter fot _top
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    // getter for _bottom
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    // setter for top
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    // setter for bottom
+    public void SetBottom( int bottom)
+    {
+        _bottom = bottom;
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,37 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        // using first constructor
+        Fraction f1 = new Fraction();
+        Console.WriteLine($"Fraction 1: {f1.GetFractionString()}");
+        Console.WriteLine(f1.GetDecimaValue());
+
+        // using second constructor
+        Fraction f2 = new Fraction(6);
+        Console.WriteLine($"Fraction 2: {f2.GetFractionString()}");
+        Console.WriteLine(f2.GetDecimaValue());
+
+        // using third constructor
+        Fraction f3 = new Fraction(6, 7);
+        Console.WriteLine($"Fraction 3: {f3.GetFractionString()}");
+        Console.WriteLine(f3.GetDecimaValue());
+
+        // using setter for top with first constructor
+        Fraction f4 = new Fraction();
+        f4.SetTop(5);
+        Console.WriteLine($"Fraction 4 (SetTop): {f4.GetFractionString()}"); //expected output 5/1
+        Console.WriteLine($"Numerator: {f4.GetTop()}");
+        Console.WriteLine(f4.GetDecimaValue());
+
+        //using setter for bottom with first constructor
+        Fraction f5 = new Fraction();
+        f5.SetBottom(4);
+        Console.WriteLine($"Fraction 5 (SetBottom): {f5.GetFractionString()}"); //expected output 1/4
+        Console.WriteLine($"Denominator: {f5.GetBottom()}");
+        Console.WriteLine(f5.GetDecimaValue());
     }
 }
