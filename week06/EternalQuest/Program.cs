13dbfdfeffa896728dee2af9c793a784692649a3
francisco aguirre <faguirreramirez@byupathway.edu>
2025-04-10 22:45:17 +0200

    eternal Quest app

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,9 +1,11 @@
 using System;
-
+// to exeed requirements I have added a leveloing up sistem
+// Every 1000 points, the program displays a “⭐ LEVEL UP!” message.
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        GoalManager manager = new GoalManager();
+        manager.Start();
     }
 }
